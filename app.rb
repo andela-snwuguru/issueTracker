e4dd59971abd340d1c8e6ea3116c5d55d9c9e629
Sunday Goodman <sundayguru@gmail.com>
2016-02-23 21:52:18 +0100

    Testing out seesion gem

--- app.rb
+++ app.rb
@@ -1,7 +1,27 @@
 require 'sinatra'
+require 'firebase'
+require 'json'
+require 'sanitize'
+require 'sinatra/session'
+
+set :session_fail, '/login'
+set :session_secret, 'ATincketdIsseueTralckera'
 
 get '/' do
-
+session!
   erb :index
   
+end
+
+get '/login' do
+  if session?
+    redirect '/'
+  else
+    'login here'
+  end
+end
+
+get '/logout' do
+  session_end!
+  redirect '/'
 end
