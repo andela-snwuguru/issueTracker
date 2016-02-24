c63ef22fad5087228959d86db3ae0d19e00cd88a
Sunday Goodman <sundayguru@gmail.com>
2016-02-24 14:22:44 +0100

    added links to side navigation, designed error page

--- app.rb
+++ app.rb
@@ -9,37 +9,27 @@ set :session_secret, 'ATincketdIsseueTralckera'
 
 require_relative 'src/authentication'
 
-get '/' do
-session!
-  erb :index
+def get_view view
+  @views = {
+  'dashboard'=> :index,
+  'department' => :department,
+  'login' => :login,
+  'signup' => :signup,
+  'recover' => :recover,
+  'error' => :error,
+  'ticket' => :ticket,
+}
+  @views.each{|key,value|  
+    return @views[view] if key == view
+  }
+  :error
 end
 
-get '/login' do
-  if session?
-    redirect '/'
-  else
-    erb :login
-  end
+get '/:view' do
+  session!
+  view = get_view params[:view]
+  erb view
 end
-
-
-get '/signup' do
-  if session[:login]
-    redirect '/'
-  else
-    erb :signup
-  end
-end
-
-
-get '/recover' do
-  if session[:login]
-    redirect '/'
-  else
-    erb :recover
-  end
-end
-
 
 get '/logout' do
   session_end!
