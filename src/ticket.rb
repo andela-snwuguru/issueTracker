b0871fe10d964a6f7fd0b851f270e1d7bf0a9711
Sunday Goodman <sundayguru@gmail.com>
2016-02-25 15:55:38 +0100

    added ticket class and modified config.rb, also implement dynamic content to new_ticket form

---
+++ src/ticket.rb
@@ -0,0 +1,48 @@
+module Guru
+  class Ticket
+
+    def initialize
+      @fb = Guru::FirebaseUtil.new
+      @fields = [
+        'title',
+        'department',
+        'priority',
+        'description',
+        'status',
+      ]
+    end
+
+    def create data
+      data['status'] = Guru::Config::STATUS_PENDING
+      record = get_data data
+      if @fb.push('ticket',record)
+        return true
+      end
+      false
+    end
+
+    def delete id
+      if @fb.delete("ticket/#{id}")
+        return true
+      end
+      false
+    end
+
+    def self.list
+      @fb = Guru::FirebaseUtil.new
+      result = @fb.fetch('ticket')
+      result ? result : {}
+    end
+
+    private
+    def get_data data
+      record = {}
+      @fields.each{|key|
+        record[key] = data[key]
+      }
+      record
+    end
+
+  end
+
+end
